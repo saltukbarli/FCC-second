f0beda473b731cb1041b2f5827fc456e5979431c
Saltuk Taha BARLI <116064848+saltukbarli@users.noreply.github.com>
2023-02-03 17:05:05 +0300

    solution

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function(req, res) {
+  let ipAdd = req.ip;
+  let usLang = req.acceptsLanguages();
+  let soft = req.hostname;
+
+  res.json({ ipaddress: ipAdd, language: usLang, software: soft });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
